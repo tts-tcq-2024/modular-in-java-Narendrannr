f2aa5aeafbe0c81a4cb95b4e11871e64283227c1
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-07-25 11:53:26 +0530

    Update ColorCoder.java

--- colorcoder/ColorCoder.java
+++ colorcoder/ColorCoder.java
@@ -2,60 +2,6 @@ package colorcoder;
 
 public class ColorCoder {
 
-    public enum MajorColor {
-        WHITE(0),
-        RED(1),
-        BLACK(2),
-        YELLOW(3),
-        VIOLET(4);
-
-        private int index;
-
-        MajorColor(int index) {
-            this.index = index;
-        }
-
-        int getIndex() {
-            return index;
-        }
-
-        public static MajorColor fromIndex(int index) {
-            for (MajorColor color : MajorColor.values()) {
-                if (color.getIndex() == index) {
-                    return color;
-                }
-            }
-            return null;
-        }
-    }
-
-    public enum MinorColor {
-        BLUE(0),
-        ORANGE(1),
-        GREEN(2),
-        BROWN(3),
-        SLATE(4);
-
-        private int index;
-
-        MinorColor(int index) {
-            this.index = index;
-        }
-
-        int getIndex() {
-            return index;
-        }
-
-        public static MinorColor fromIndex(int index) {
-            for (MinorColor color : MinorColor.values()) {
-                if (color.getIndex() == index) {
-                    return color;
-                }
-            }
-            return null;
-        }
-    }
-
     private static final String[] majorColorNames = {
             "White", "Red", "Black", "Yellow", "Violet"
     };
